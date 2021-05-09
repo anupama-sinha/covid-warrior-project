f725decccb8baea69bbf28346ff79f9ae84af195
Vicky AV <vickyavw.10@gmail.com>
2021-05-09 10:44:55 +0530

    project restructure

---
+++ src/main/java/com/covid/fight/warriorproject/WarriorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.covid.fight.warriorproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarriorProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarriorProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covid/fight/warriorproject/WarriorProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid.fight.warriorproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarriorProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
